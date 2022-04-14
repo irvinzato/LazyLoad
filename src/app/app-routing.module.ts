4b34cc4cb7822340c8199d49eca86cd8f75302ff
irvinzato <multizato@hotmail.com>
2022-04-14 00:12:14 -0500

    Se hicieron diferentes modulos con la forma correcta de ordenarlos dentro del proyecto y utilizando rutas hijas, forma correcta de utilizar lazy load

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    //ASI LE DECIMOS QUE LEA EL MODULO QUE ESTA EN LA CARPETA "auth" y este conecta con sus rutas
+    //es una promesa que cuando se resuelve ".then()" regresa el modulo "m"
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)    
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule)
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    //component: ErrorPageComponent
+    redirectTo: '404'
+  }
+  
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,37 @@
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { Router, Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'registro',
+        component: RegistroComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AuthRoutingModule { }
